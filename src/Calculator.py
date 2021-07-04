6a2f6b46a5c332dd2585910289dc8a6ad78a7069
Parikshit Narang <parikshit3097@gmail.com>
2021-07-04 18:03:42 -0400

    Task: Created class in calculator.py for different operations like add, sub, mul, div, sq, sq root

---
+++ src/Calculator.py
@@ -0,0 +1,36 @@
+class Calculator:
+    result = 0
+
+    #constructor
+    def __init__(self):
+        pass
+
+    #add method
+    def add(self, a, b):
+        self.result = a + b
+        return self.result
+
+    #sub method
+    def subtract(self, a, b):
+        self.result = a - b
+        return self.result
+
+    #mul method
+    def multiply(self, a, b):
+        self.result = a * b
+        return self.result
+
+    #div method
+    def divide(self, a, b):
+        self.result = a / b
+        return self.result
+
+    #sq method
+    def square(self, a):
+        self.result = a * a
+        return self.result
+
+    #sq root method
+    def sqrt(self, a):
+        self.result = pow(a, 0.5)
+        return self.result
