a2a56a5bf97269fd8e480b36ff08c37136bba5b9
Parikshit Narang <pn24@njit.edu>
2021-07-05 20:39:51 -0400

    Task: added @classmethod annotation in Calculator.py

--- src/Calculator.py
+++ src/Calculator.py
@@ -14,31 +14,37 @@ class Calculator:
         pass
 
     #add method
+    @classmethod
     def add(self, a, b):
         self.result = Addition.add(a, b)
         return self.result
 
     #sub method
+    @classmethod
     def subtract(self, a, b):
         self.result = Subtraction.sub(a, b)
         return self.result
 
     #mul method
+    @classmethod
     def multiply(self, a, b):
         self.result = Multiplication.mul(a, b)
         return self.result
 
     #div method
+    @classmethod
     def divide(self, a, b):
         self.result = Division.div(a, b)
         return self.result
 
     #sq method
+    @classmethod
     def square(self, a):
         self.result = Squaring.squaring(a)
         return self.result
 
     #sq root method
+    @classmethod
     def sqrt(self, a):
         self.result = SquareRoot.square_root(a)
         return self.result
