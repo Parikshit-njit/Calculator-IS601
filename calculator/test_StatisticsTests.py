0926120e908921ae54cacb20037528dc18ecd0a0
Parikshit Narang <pn24@njit.edu>
2021-07-08 16:39:25 -0400

    Feature: Added function in test_StatisticsTests.py to test mode operation

--- calculator/test_StatisticsTests.py
+++ calculator/test_StatisticsTests.py
@@ -14,6 +14,15 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(statistics.get_mean(data), statistics.result)
         print("Mean test cases passed!")
 
+    def test_mode_result(self):
+        data = []
+        statistics = Statistics();
+        test_cases = CsvReader("TestFiles/Unit Test Mode.csv").data
+        for test_case in test_cases:
+            data.append(int(test_case['Value']))
+        self.assertEqual(statistics.get_mode(data), statistics.result)
+        print("Mode test cases passed!")
+
 
 if __name__ == '__main__':
     unittest.main()
