2dbe64f4e18b9efe20a872668dbe3d6534ba5885
Parikshit Narang <pn24@njit.edu>
2021-07-07 23:11:26 -0400

    Task: added import for mean in Statistics.py and created unit test for Statistics

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -1,6 +1,11 @@
 from Calculator.Calculator import Calculator
+from Mean import mean
 
 
 class Statistics(Calculator):
     def __init__(self):
         super().__init__()
+
+    def get_mean(self):
+        self.result = mean(self.mean_list)
+
